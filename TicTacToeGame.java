8162c6d9623649a38e4ee046ab947e9cabaa5513
Atharva <atharva.tathe55@gmail.com>
2021-04-05 14:02:02 +0530

    Added : GameBoard in Code

---
+++ TicTacToeGame.java
@@ -0,0 +1,25 @@
+public class TicTacToeGame
+{
+
+	public char board[] = new char[10] ;
+
+
+	public void assignToBoard()
+	{
+		for(int i=1; i<10 ;i++)
+		{
+			board[i]= 'a';
+			System.out.println("board:["+i+"]="+board[i]);
+		}
+
+
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to Tic Tac Toe game ");
+		TicTacToeGame t1 = new TicTacToeGame();
+		t1.assignToBoard();
+
+	}
+}
